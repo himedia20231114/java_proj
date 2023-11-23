fbaf654007fb521050a9e082267e31a57d832cae
Jangwoo Park <p.jangwoo@gmail>
2023-11-23 18:19:34 +0900

    11월 23일 - 저녁 (switch)

--- src/chap04/ex01/If_Statement.java
+++ src/chap04/ex01/If_Statement.java
@@ -19,7 +19,7 @@ public class If_Statement {
 		  	 	
 		  	 if -- 3가지 구문 
 		  	 
-		  	 1.  조건이 1개일때  
+		  	 1.  조건이 1개일때     <==== 삼항 연산자로 수정      (조건) ? 참 : 거짓 ; 
 		  	 
 		  	      if (조건) {
 		  	          조건이 true 일때 실행문 ; 
@@ -47,15 +47,72 @@ public class If_Statement {
 		 
 		 */
 		
+		
+		// 조건이 1 개일때 : if ~ else ~ 
 		int a = 10 ; 
 		
 		if ( a < 30) {
+			
 			System.out.println(" 참입니다. ");	// 조건이 참이면 실행
+			
+			System.out.println("참일때 또 실행 됩니다. ");
+			
 		}else {
 			System.out.println(" 거짓입니다. ");	// 조건이 거짓이면 실행 
 		}
 		
+		// 조건이 여러개 일때   : 점수가 90 이상일때 : A 학점 , 80점이상일때 : B 학점, 70점이상일때 : C 
+		
+		System.out.println("===================");
+		
+		int score = 80 ;
+		
+		if ( score >= 90) {
+			System.out.println("A 학점입니다. ");
+		}else if (score >= 80) {
+			System.out.println("B 학점입니다. ");
+		}else if (score >= 70 ) {
+			System.out.println("C 학점입니다. ");
+		}else if ( score >=60 ) {
+			System.out.println("D 학점입니다. ");
+		}else {
+			System.out.println("F 학점입니다. ");
+		}
+		
+		System.out.println("=====if 조건에서 조건처리 =====================");
+		
+		 score = 90 ;
+		
+		if ( score >= 60 && score < 70) { 			// true && false  ==> false
+			System.out.println("D 학점입니다. ");
+		}else if (score >= 70 && score < 80) {		//true && false ==> false
+			System.out.println("C 학점입니다. ");
+		}else if (score >= 80 && score < 90) {		//true && false ==> false
+			System.out.println("B 학점입니다. ");
+		}else if ( score >=90 && score < 100) {		// true && true ==> true 
+			System.out.println("A 학점입니다. ");
+		}else {
+			System.out.println("F 학점입니다. ");
+		}
+		
+		System.out.println("======================");
+		
+		
+		// 조건이 true 일때만 실행  : 중괄호를 넣어도 되고 생략 될수도 있다. 
+		
+		if (10 > 5) { System.out.println("참입니다"); } 
+		
+		if (10 > 5)  System.out.println("10은 5보다 큽니다."); 
+		
+		
+		
+		
+		
 		System.out.println("프로그램 종료입니다. ");
+		
+		
+		
+		
 		
 		
 
