26454252a5f33d39f39584e1d13a1acd578cd3ab
Jangwoo Park <p.jangwoo@gmail>
2023-11-30 16:26:44 +0900

    11월 30일 - 16시

--- src/lab/chap17/account/Account.java
+++ src/lab/chap17/account/Account.java
@@ -7,11 +7,11 @@ public class Account {
 	int money = 10000 ; 		// 자신 돈 
 	
 
-	void incom (int money) {   // 입금 <== 00 입금되었습니다. 계좌의 총금액 00 입니다. 
+	void deposit (int money) {   // 입금 <== 00 입금되었습니다. 계좌의 총금액 00 입니다. 
 		
 	}
 	
-	void deposit ( int money ) {   // 출금  <== 00원 출금 했습니다. 잔고는 00 원 있습니다. 
+	void withdraw ( int money ) {   // 출금  <== 00원 출금 했습니다. 잔고는 00 원 있습니다. 
 		
 	}
 
